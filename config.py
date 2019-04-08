f2414d6b8ea7ede09182b578ec85b25af04304b5
levi1996 <levi@cinnamon.is>
2019-04-08 21:37:31 +0700

    sth

--- config.py
+++
@@ -1,7 +0,0 @@
-# for dot acess
-# arg = {'name': 'jojonki', age: 100}
-# conf = Config(**arg)
-# print(conf.name) ==> 'jojonki'
-class Config(object):
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
--- squad/README.md
+++
@@ -1,2 +0,0 @@
-This software includes the work that is distributed in the Apache License 2.0.
-https://github.com/allenai/bi-att-flow
